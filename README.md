18fce265672fa0f4adf69905d57584b15b641f13
Hardika <patel33hardik@hotmail.com>
2023-08-22 15:54:47 +1000

    1. Resume extractor initial development
    Notes: Please check README.MD file

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# ExtractResumeByKeywords
-The script is designed to extract resumes by searching for keywords across all files.
+# Resume Extractor
+
+The **Resume Extractor** is a Python script that enables you to search for specific keywords within a collection of resumes stored as PDF files. This tool is designed to help streamline the process of identifying resumes that align with specific skills or qualifications.
+
+
+## Introduction
+
+When dealing with a large number of resumes, it can be time-consuming to manually review each one to find candidates with specific skills. The **Resume Extractor** script automates this process by allowing you to specify keywords, searching through the resumes, and presenting a list of matching resumes along with the corresponding keywords.
+
+## Features
+
+- Search for multiple keywords within resumes.
+- Display detailed information about matching resumes.
+- User-friendly command line interface.
+- Case-insensitive keyword search.
+
+## Usage
+
+### Requirements
+
+- Python 3.x
+
+### Installation
+
+1. Clone or download the repository.
+2. Open a terminal or command prompt.
+3. Navigate to the directory containing the `resumeExtractor.py` script.
+4. Install any required packages using `pip` if necessary:
+
+### Command Line Interface
+
+To use the script, open a terminal or command prompt and navigate to the directory containing the `resumeExtractor.py` script. Run the script using the following command format:
+
+```bash
+python resumeExtractor.py -k KEYWORDS -p FILES_PATH
+```
+
+### Example
+Suppose you have a directory named demo_resumes/ containing various resumes in PDF format.
+You want to search for resumes containing keywords "python" and "js". Run the following command:
+
+```bash
+$ py resumeExtractor.py -k python,js -p demo_resumes/
+
+***Following are shortlisted resumes.***
+
+1. Filename: Resume1.pdf,
+Matching keywords: python, js
+
+2. Filename: Resume2.pdf,
+Matching keywords: python, js
+
+3. Filename: Resume3.pdf,
+Matching keywords: python
+
+4. Filename: Resume4.pdf,
+Matching keywords: js
+```
+
+### License
+This project is licensed under the MIT License.
---
+++ resumeExtractor.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+import PyPDF2
+import re
+import sys
+
+
+def print_colored(text, color_code):
+    print(f"\033[{color_code}m{text}\033[0m")
+
+def printGreen(text):
+    print_colored(text, '32')
+
+def printRed(text):
+    print_colored(text, '31')
+
+# Function to search for keywords in text
+def search_keywords(text, keywords):
+    match_keys = []
+    for keyword in keywords:
+        pattern = r'\b{}(?:,)?\b'.format(keyword)
+        if re.search(pattern, text, re.IGNORECASE):
+            match_keys.append(keyword)
+
+    return match_keys
+
+
+def main():
+    parser =argparse.ArgumentParser()
+    parser.add_argument(
+        '-k', '--keywords', required=True, help='Please enter at least one keyword. e.x. python, js'
+    )
+    parser.add_argument(
+        '-p', '--files_path', default='demo_resume', help='Add files location to be extracted'
+    )
+    args = parser.parse_args()
+
+    if args.files_path is None or not os.path.exists(args.files_path):
+        printRed('Please provide a valid path..')
+        parser.print_help()
+        exit(1)
+
+    if args.keywords is None:
+        printRed('Please Enter a valid keyword with the comma..')
+        parser.print_help()
+        exit(1)
+
+
+    keyword_list = args.keywords.split(',')
+    shortlisted_resumes = []
+    # Iterate through PDF files in the directory
+    for filename in os.listdir(args.files_path):
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(args.files_path, filename)
+
+            # Read the PDF content
+            with open(pdf_path, 'rb') as pdf_file:
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
+                resume_text = ""
+                for page_num in range(len(pdf_reader.pages)):
+                    page = pdf_reader.pages[page_num]
+                    resume_text += page.extract_text()
+
+                matched_keywords = search_keywords(resume_text, keywords=keyword_list)
+                if len(matched_keywords):
+                    shortlisted_resumes.append({
+                        'FileName': filename,
+                        'MatchKeywords': ', '.join(matched_keywords)
+                    })
+
+    if len(shortlisted_resumes) > 0:
+        print("\n***Following are shortlisted resumes.***\n")
+        for index, resume in enumerate(shortlisted_resumes, start=1):
+            printGreen(
+                f'{index}. Filename: {resume["FileName"]}, \nMatching keywords: {resume["MatchKeywords"]}\n'
+            )
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except Exception as ex:
+        print(f'Exception: {ex}')
+        sys.exit(1)
